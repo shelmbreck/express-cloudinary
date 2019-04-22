b2f100831f14019e6c2aa59c9d1ee28941e3121f
shelmbreck <samantha.helmbreck@gmail.com>
2019-04-22 11:31:22 -0700

    adding solution

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-var express = require('express');
-var ejsLayouts = require('express-ejs-layouts');
-var app = express();
+require('dotenv').config()
 
+// Require needed modules
+let cloudinary = require('cloudinary')
+let express = require('express');
+let layouts = require('express-ejs-layouts');
+let multer = require('multer')
+
+// Set up multer's storage/memory locations
+let storage = multer.diskStorage({
+  filename: (req, file, cb) => {
+    cb(null, `${file.fieldname}-${Date.now()}`)
+
+  },
+  destination: (req, file, cb) => {
+    cb(null, '/tmp/my-uploads')
+  }
+})
+let upload = multer({ storage: storage })
+
+//Declare express app
+let app = express();
+
+// Setting and middleware
 app.set('view engine', 'ejs');
-app.use(ejsLayouts);
+app.use(layouts);
 
-app.get('/', function(req, res) {
-  res.render('index');
+app.get('/', (req, res) => {
+  res.render('index', {recentUpload: '' });
 });
+
+// POST / A place tp send the image data
+app.post('/', upload.single('myFile'), (req, res) => {
+  cloudinary.uploader.upload(req.file.path, result => {
+    res.render('index', { recentUpload: result.secure_url})
+  })
+})
 
 app.listen(3000);
